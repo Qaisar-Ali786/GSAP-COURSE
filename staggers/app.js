fabdcd59b072fce80b0fdba696171d9250f755c0
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2025-12-31 22:44:11 -0800

    Staggers

---
+++ staggers/app.js
@@ -0,0 +1,33 @@
+// gsap.set(".box", {
+//   borderRadius: 0,
+// });
+
+// gsap.to(".box", {
+//   borderRadius: 100,
+//   duration: 1,
+//   y: -100,
+//   ease: "power1",
+//   yoyo: true,
+//   repeat: -1,
+//   stagger: {
+//     amount: 4,
+//     each: 0.5,
+//     from: 0,
+//   },
+// });
+
+gsap.set(".box", {
+  borderRadius: 0,
+});
+
+gsap.to(".box", {
+  borderRadius: 100,
+  y: -100,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+  stagger: {
+    amount: 4,
+    each: 0.5,
+  },
+});
